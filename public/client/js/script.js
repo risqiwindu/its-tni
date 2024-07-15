f5ca45818c2f9a567f18d736dbdde6b7b1a10f01
SANDI KOMARA <sandikomara01@gmail.com>
2024-07-15 10:56:30 +0700

    update

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -1,174 +1,193 @@
-const video = document.getElementById("video");
-let canvas;
-let isVideoPlaying = false;
-let lastNotificationTime = 0;
-let isLoading = true;
+document.addEventListener("DOMContentLoaded", function() {
+  const video = document.getElementById("video");
+  const stopButton = document.getElementById("stop-button");
+  const loader = document.getElementById("loader");
 
-Promise.all([
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/its-tni/public/client/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/its-tni/public/client/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/its-tni/public/client/models"),
-  faceapi.nets.ageGenderNet.loadFromUri("/its-tni/public/client/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/its-tni/public/client/models"),
-]).then(startWebcam);
-
-function startWebcam() {
-  navigator.mediaDevices
-    .getUserMedia({
-      video: true,
-      audio: false,
-    })
-    .then((stream) => {
-      video.srcObject = stream;
-      video.play();
-      isVideoPlaying = true;
-
-      video.style.display = "block";
-      isLoading = false;
-    })
-    .catch((error) => {
-      console.error(error);
+  console.log("Loading models...");
+  Promise.all([
+    faceapi.nets.ssdMobilenetv1
+      .loadFromUri("/its-tni/public/client/models")
+      .then(() => console.log("Loaded ssdMobilenetv1")),
+    faceapi.nets.faceRecognitionNet
+      .loadFromUri("/its-tni/public/client/models")
+      .then(() => console.log("Loaded faceRecognitionNet")),
+    faceapi.nets.faceExpressionNet
+      .loadFromUri("/its-tni/public/client/models")
+      .then(() => console.log("Loaded faceExpressionNet")),
+    faceapi.nets.ageGenderNet
+      .loadFromUri("/its-tni/public/client/models")
+      .then(() => console.log("Loaded ageGenderNet")),
+    faceapi.nets.faceLandmark68Net
+      .loadFromUri("/its-tni/public/client/models")
+      .then(() => console.log("Loaded faceLandmark68Net")),
+  ])
+    .then(getLabeledFaceDescriptions)
+    .then((labeledDescriptors) => startWebcam(labeledDescriptors))
+    .catch((e) => {
+      console.error("Failed to load models:", e);
+      loader.innerText = "Failed to load models";
     });
-}
 
-function getLabeledFaceDescriptions(label) {
-  return Promise.all(
-    Array.from({ length: 2 }, (_, i) => {
-      const imgPath = `/its-tni/public/client/labels/${label}/${i}.jpg`;
-      return faceapi.fetchImage(imgPath).then(async (img) => {
-        const detection = await faceapi
-          .detectSingleFace(img)
-          .withFaceLandmarks()
-          .withFaceExpressions()
-          .withAgeAndGender()
-          .withFaceDescriptor();
-        return detection.descriptor;
-      });
-    })
-  ).then((descriptors) => {
-    return new faceapi.LabeledFaceDescriptors(label, descriptors);
-  });
-}
-
-function showNotification(message, videoId) {
-  const notification = document.createElement("div");
-  notification.className = "notification";
-  
-  const iframe = document.createElement("iframe");
-  iframe.width = "560";
-  iframe.height = "315";
-  iframe.src = `https://www.youtube.com/embed/${videoId}`;
-  iframe.frameBorder = "0";
-  iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-  iframe.allowFullscreen = true;
-
-  notification.appendChild(iframe);
-  document.body.appendChild(notification);
-
-  // Mengatur posisi notifikasi di tengah layar
-  const windowHeight = window.innerHeight;
-  const notificationHeight = notification.offsetHeight;
-  notification.style.top = `${(windowHeight - notificationHeight) / 2}px`;
-
-  setTimeout(() => {
-    notification.remove();
-  }, 2 * 60 * 1000);
-}
-
-const loadingOverlay = document.createElement("div");
-loadingOverlay.className = "loading-overlay";
-loadingOverlay.innerText = "Loading..."; // Pesan loading bisa disesuaikan
-
-document.body.appendChild(loadingOverlay);
-
-video.addEventListener("play", async () => {
-  if (isLoading) {
-    // Tampilkan elemen loading saat proses inisialisasi
-    loadingOverlay.style.display = "flex";
+  async function getLabeledFaceDescriptions() {
+    const labels = ["sandi"];
+    console.log("Getting labeled face descriptions...");
+    return Promise.all(
+      labels.map(async (label) => {
+        const descriptions = [];
+        for (let i = 1; i <= 2; i++) {
+          const img = await faceapi.fetchImage(
+            `/its-tni/public/client/labels/${label}/${i}.jpg`
+          );
+          const detections = await faceapi
+            .detectSingleFace(img)
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withAgeAndGender()
+            .withFaceDescriptor();
+          if (detections) {
+            descriptions.push(detections.descriptor);
+          }
+        }
+        return new faceapi.LabeledFaceDescriptors(label, descriptions);
+      })
+    );
   }
-  
-  const label = "sandi"; // Ganti dengan label yang diinginkan
 
-  try {
-    const labeledFaceDescriptors = await getLabeledFaceDescriptions(label);
-    const faceMatcher = new faceapi.FaceMatcher([labeledFaceDescriptors]);
+  let emotionData = {
+    neutral: 0,
+    happy: 0,
+    sad: 0,
+    angry: 0,
+    fearful: 0,
+    disgusted: 0,
+    surprised: 0,
+    count: 0,
+  };
 
-    if (canvas) {
-      canvas.remove();
-    }
+  function startWebcam(labeledDescriptors) {
+    console.log("Starting webcam...");
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
+          console.log("Webcam started");
+          video.play();
+          initializeDetection(labeledDescriptors);
+        };
+      })
+      .catch((error) => {
+        console.error("Error accessing the webcam", error);
+        loader.innerText = "Error accessing the webcam";
+      });
+  }
 
-    const test = document.getElementById('test');
-    canvas = faceapi.createCanvasFromMedia(video);
-    canvas.style.position = 'absolute';
-    test.append(canvas);
-
+  function initializeDetection(labeledDescriptors) {
+    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.getElementById("test").appendChild(canvas);
     const displaySize = { width: video.width, height: video.height };
     faceapi.matchDimensions(canvas, displaySize);
 
-    const interval = setInterval(async () => {
-      const detections = await faceapi
+    function processVideoFrame() {
+      faceapi
         .detectAllFaces(video)
         .withFaceLandmarks()
         .withFaceExpressions()
         .withAgeAndGender()
-        .withFaceDescriptors();
-
-      const resizedDetections = faceapi.resizeResults(
-        detections,
-        displaySize
-      );
-
-      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-
-      resizedDetections.forEach((detection) => {
-        const bestMatch = faceMatcher.findBestMatch(detection.descriptor);
-        if (bestMatch.label === label) {
-          const box = detection.detection.box;
-          const age = Math.round(detection.age);
-          const labelInfo = `${bestMatch.toString()} - Age: ${age}`;
-
-          const drawBox = new faceapi.draw.DrawBox(box, {
-            label: labelInfo,
-          });
-          drawBox.draw(canvas);
-
-          faceapi.draw.drawFaceExpressions(canvas, detection);
-
-          const highestEmotion = Object.keys(detection.expressions).reduce(
-            (a, b) =>
-              detection.expressions[a] > detection.expressions[b] ? a : b
+        .withFaceDescriptors()
+        .then((detections) => {
+          const resizedDetections = faceapi.resizeResults(
+            detections,
+            displaySize
           );
+          canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+          faceapi.draw.drawDetections(canvas, resizedDetections);
 
-          if (highestEmotion === "happy" && isVideoPlaying) {
-            const currentTime = Date.now();
-            if (currentTime - lastNotificationTime > 2 * 60 * 1000) { // Check if 2 minutes have passed since last notification
-              showNotification("User is feeling happy! Watch this fun video:", "3df8CooYnSs?si=hwzDF0uP4uvvf1GX&amp;controls=0");
-              lastNotificationTime = currentTime;
-
-              // Stop the video
-              const stream = video.srcObject;
-              const tracks = stream.getTracks();
-              tracks.forEach(track => track.stop());
-              video.srcObject = null;
-              isVideoPlaying = false;
-
-              // Clear the canvas
-              canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-
-              // Restart the video after 2 minutes
-              setTimeout(() => {
-                startWebcam();
-              }, 2 * 60 * 1000); // 2 minutes in milliseconds
+          resizedDetections.forEach((detection) => {
+            const { age, expressions } = detection;
+            const maxEmotion = Object.keys(expressions).reduce((a, b) =>
+              expressions[a] > expressions[b] ? a : b
+            );
+            const result = faceMatcher.findBestMatch(detection.descriptor);
+            let displayName = result.toString(); // Default to the matched label
+            if (result.label === "sandi") {
+              displayName = "sandi"; // Customize display name if the label is 'fadil'
             }
+            const text = `${displayName}, ${age.toFixed(
+              0
+            )} years old, ${maxEmotion}`;
+            const anchor = {
+              x: detection.detection.box.bottomLeft.x,
+              y: detection.detection.box.bottomLeft.y + 6,
+            };
+            const boxHeight = detection.detection.box.height;
+            new faceapi.draw.DrawTextField(
+              [text],
+              anchor,
+              boxHeight * 0.5
+            ).draw(canvas);
+            updateEmotionData(expressions);
+          });
+
+          if (detections.length > 0) {
+            loader.style.display = "none";
           }
+          requestAnimationFrame(processVideoFrame);
+        })
+        .catch((error) => {
+          console.error("Error processing video frame:", error);
+          requestAnimationFrame(processVideoFrame);
+        });
+    }
+
+    requestAnimationFrame(processVideoFrame);
+  }
+
+  function updateEmotionData(expressions) {
+    Object.keys(expressions).forEach((key) => {
+      if (emotionData.hasOwnProperty(key)) {
+        // Check if the key exists in emotionData
+        emotionData[key] += expressions[key];
+      }
+    });
+    emotionData.count++; // Increment the count of processed faces
+  }
+
+  stopButton.addEventListener("click", () => {
+    video.pause();
+    video.srcObject.getTracks().forEach((track) => track.stop());
+    document.getElementById("test").style.display = "none";
+    document.getElementById("results-page").style.display = "block";
+    analyzeEmotions();
+  });
+
+  function analyzeEmotions() {
+    if (emotionData.count > 0) {
+      const percentages = Object.keys(emotionData).reduce((acc, cur) => {
+        if (cur !== "count") {
+          acc[cur] =
+            ((emotionData[cur] / emotionData.count) * 100).toFixed(2) + "%";
         }
-      });
-      loadingOverlay.style.display = "none";
-      isLoading = false;
-    }, 1000);
-  } catch (error) {
-    console.error("Error in face detection:", error);
-    loadingOverlay.style.display = "none"; // Sembunyikan elemen loading jika terjadi kesalahan
-    isLoading = false;
+        return acc;
+      }, {});
+      displayResults(percentages);
+    } else {
+      console.log("No emotions detected.");
+    }
+  }
+
+  function displayResults(percentages) {
+    const tbody = document
+      .getElementById("results-table")
+      .getElementsByTagName("tbody")[0];
+    tbody.innerHTML = "";
+    Object.entries(percentages).forEach(([emotion, percentage]) => {
+      const row = tbody.insertRow();
+      const cellEmotion = row.insertCell(0);
+      const cellPercentage = row.insertCell(1);
+      cellEmotion.textContent = emotion;
+      cellPercentage.textContent = percentage;
+    });
   }
 });
